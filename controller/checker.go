e72400d7b72312aea534d51bc904d8895b31bfe9
Just1689 <Just1689@gmail.com>
2022-02-09 11:25:06 +0200

    Catch CMD errors as unhealthy state. Added comments

--- controller/checker.go
+++ controller/checker.go
@@ -5,9 +5,13 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// Check compares the output from the command to the list of error words
 func Check(c *model.Config) (healthy bool, err error) {
 	var in string
 	in, err = CMD(c.CMD, c.Args)
+	if err != nil {
+		return
+	}
 	healthy, err = EvalFunction(in, c.ErrorWords)
 	logrus.WithField("healthy", healthy).Infoln("> Starting Check")
 	return
