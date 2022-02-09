a366fdb684b62a36c1fc69c89c2fb320e3936ac2
Just1689 <Just1689@gmail.com>
2022-02-09 11:24:38 +0200

    Comments

--- controller/cron.go
+++ controller/cron.go
@@ -4,6 +4,7 @@ import (
 	"github.com/robfig/cron"
 )
 
+// InitiateCron starts the cron that runs the function
 func InitiateCron(cronString string, f func()) {
 	c := cron.New()
 	c.AddFunc(cronString, f)
